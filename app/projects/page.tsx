6b2ed3a4e75d5ad621d06a3f27382a6acbf22011
IssamElJaouhari <issamefar58@gmail.com>
2025-04-07 16:17:55 +0100

    Refactor AboutPage component to simplify structure and remove unnecessary sections

---
+++ app/projects/page.tsx
@@ -0,0 +1,11 @@
+// app/contact/page.tsx
+
+export default function ProjectPage() {
+    return (
+      <div>
+        <h1>Project Page</h1>
+        <p>This is the Project page content.</p>
+      </div>
+    );
+  }
+  
